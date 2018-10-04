f6a07af0a1d15bf07617bba8e89ae2f341ccc7af
William Fischer <william.fischer42@gmail.com>
2018-10-04 12:59:17 -0600

    Created & tested model associations

--- app/models/comedian.rb
+++ app/models/comedian.rb
@@ -1,5 +1,6 @@
 class Comedian < ActiveRecord::Base
+  has_many :specials
   validates_presence_of :name, :birth_date
+
   # TODO: Create custom validation for birth date (date object format)
-  
 end
--- spec/models/comedian_spec.rb
+++ spec/models/comedian_spec.rb
@@ -1,12 +1,20 @@
 RSpec.describe Comedian do
-  describe 'Validation: invalid if' do
-    it 'name is missing' do
-      comic = Comedian.create(birth_date: Date.today())
-      expect(comic).to_not be_valid
+  describe 'Validations:' do
+    describe 'invalid if' do
+      it 'name is missing' do
+        comic = Comedian.create(birth_date: Date.today())
+        expect(comic).to_not be_valid
+      end
+
+      it 'birth_date is missing' do
+        comic = Comedian.create(name: 'Mitch Hedberg')
+        expect(comic).to_not be_valid
+      end
     end
-    it 'birth_date is missing' do
-      comic = Comedian.create(name: 'Mitch Hedberg')
-      expect(comic).to_not be_valid
+
+    it 'has_many specials' do
+      association = described_class.reflect_on_association(:specials)
+      expect(association.macro).to eq :has_many
     end
   end
 end
